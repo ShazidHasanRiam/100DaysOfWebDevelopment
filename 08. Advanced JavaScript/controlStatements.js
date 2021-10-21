d3985858ced961f3f91341f160eb669431e011dc
ShazidHasanRiam <shazid.riam@northsouth.edu>
2021-10-21 21:04:42 +0600

    More JavaScript

---
+++ 08. Advanced JavaScript/controlStatements.js
@@ -0,0 +1,19 @@
+//While Loop
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
+
+//For Loop
+//structure
+for ( var i =1 ; i<9 ; i++){
+    console.log(i);
+}
