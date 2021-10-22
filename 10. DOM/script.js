9cb166b259a97d6ce2b74b65bb09a589d6e0a029
ShazidHasanRiam <shazid.riam@northsouth.edu>
2021-10-22 21:02:13 +0600

    DOM Update

--- 10. DOM/script.js
+++ 10. DOM/script.js
@@ -1,1 +1,14 @@
+//javascript working test
 alert("Hello! JS is working")
+
+// get element by tag name
+document.getElementsByTagName("li")[1].style.color = "red";
+
+//get element by class name
+document.getElementsByClassName("btn")[0].style.color = "red";
+
+//get element by id
+document.getElementById("title").innerHTML = "Good Bye";
+
+//query selector
+document.querySelector("#title").style.color = "red";
