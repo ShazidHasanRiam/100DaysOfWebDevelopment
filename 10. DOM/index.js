0ac3f8bb36c4224fe221333b152e4f6f55058b20
shazidhasanriam <shazid.riam@northsouth.edu>
2021-11-10 19:09:59 +0600

    update

---
+++ 10. DOM/index.js
@@ -0,0 +1,11 @@
+// get element by tag name
+document.getElementsByTagName("li")[1].style.color = "red";
+
+//get element by class name
+document.getElementsByClassName("btn")[0].style.color = "red";
+
+//get element by id
+document.getElementById("title").innerHTML = "Good Bye";
+
+//query selector
+document.querySelector("#title").style.color = "red";
