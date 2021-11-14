7786a65b28799853fbad9829754b9d59c7cd0304
shazidhasanriam <shazid.riam@northsouth.edu>
2021-11-14 20:28:51 +0600

    update js

---
+++ 11. JS In Details/script.js
@@ -0,0 +1,16 @@
+let js = "boring";
+if (js === "boring") {
+  alert("Keep practicing js more");
+}
+alert("Pass");
+
+// Practice from variable
+let country = "Bangladesh";
+let continent = "Asia";
+let population = 160;
+
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population (in million): " + population);
+
+//data types
